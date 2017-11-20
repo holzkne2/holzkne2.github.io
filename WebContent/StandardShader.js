94ed432c38e63842ee908e0f7e2e07ff436520be
holzkne2 <holzkne2@msu.edu>
2017-11-20 08:22:43 -0500

    Pixel Lighting

--- WebContent/StandardShader.js
+++ WebContent/StandardShader.js
@@ -7,34 +7,37 @@ var vertexShaderSource =
 	uniform mat4 uPMatrix;
 	uniform mat3 uNMatrix;
 	
-	uniform vec3 uAmbientColor;
-	
-	uniform vec3 uLightingDirection;
-	uniform vec3 uDirectionalColor;
-	
 	varying vec2 vTextureCoord;
-	varying vec3 vLightWeighting;
+	varying vec3 vNormal;
 	
 	void main(void) {
 	    gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
 	    vTextureCoord = aTextureCoord;
 	    
-        vec3 transformedNormal = uNMatrix * aVertexNormal;
-        float directionalLightWeighting = max(dot(transformedNormal, uLightingDirection), 0.0);
-        vLightWeighting = uAmbientColor + uDirectionalColor * directionalLightWeighting;
+        vNormal = uNMatrix * aVertexNormal;
 	}`
 	;
 
 var fragmentShaderSource =
 	`precision mediump float;
 
+	uniform vec3 uAmbientColor;
+	
+	uniform vec3 uLightingDirection;
+	uniform vec3 uDirectionalColor;
+
     varying vec2 vTextureCoord;
-    varying vec3 vLightWeighting;
+    varying vec3 vNormal;
 
     uniform sampler2D uSampler;
 
     void main(void) {
         vec4 textureColor = texture2D(uSampler, vTextureCoord);
-        gl_FragColor = vec4(textureColor.rgb * vLightWeighting, 1);
+        
+        float light = max(dot(vNormal, uLightingDirection), 0.0);
+       	
+       	vec3 col = textureColor.rgb * (uAmbientColor + uDirectionalColor * light);
+        
+        gl_FragColor = vec4(col, 1);
     }`
 	;
